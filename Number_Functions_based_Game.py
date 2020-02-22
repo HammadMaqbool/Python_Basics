bca245f21a33e718d72c4535f42d133308a456c3
HammadMaqbool <hammad_mqbl@outlook.com>
2020-02-22 17:39:59 +0500

    doc added

--- Number_Functions_based_Game.py
+++ Number_Functions_based_Game.py
@@ -2,7 +2,7 @@ import random
 Comp_guess_number = random.randint(1,5)
 total_number_of_Attempts = 3
 cuurent_attempt =1
-
+# Note: Ffunction_name.__doc__ is a method use to read the doc string of the function.
 def remining_attempts_checker():
     """It checks every time how much attempts remaining"""
     if cuurent_attempt == total_number_of_Attempts:
