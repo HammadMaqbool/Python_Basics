219cdb4651eed1ad67b43d3961ddc0a2b3259c79
HammadMaqbool <hammad_mqbl@outlook.com>
2020-02-22 17:55:28 +0500

    IO Handling File

--- File_Handling.py
+++ File_Handling.py
@@ -8,3 +8,9 @@ file_pointer1 = open("Readme.txt","r")
 contents_we_read1 = file_pointer1.read(6)  #<here i mention 6 so it will read 6 chr only
 print(contents_we_read1)
 file_pointer1.close()
+
+#Read line one by one
+file_pointer2 = open("Readme.txt","r")
+print(file_pointer2.readline(),end="")
+print(file_pointer2.readline())
+file_pointer2.close()
