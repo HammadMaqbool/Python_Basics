48ce393f473448b2b5c246eb760a727a8899fa11
HammadMaqbool <hammad_mqbl@outlook.com>
2020-02-22 17:51:19 +0500

    IO Handling File

---
+++ File_Handling.py
@@ -0,0 +1,10 @@
+file_pointer = open("Readme.txt","r")
+contents_we_read = file_pointer.read()
+print(contents_we_read)
+file_pointer.close() #If you open a file don't forget to close it.
+
+#Read the amount of number you want
+file_pointer1 = open("Readme.txt","r")
+contents_we_read1 = file_pointer1.read(6)  #<here i mention 6 so it will read 6 chr only
+print(contents_we_read1)
+file_pointer1.close()
---
+++ Readme.txt
@@ -0,0 +1,3 @@
+This is the readme file
+This is the file we want to read from the Filehandling
+this is the 3rd line of the file
