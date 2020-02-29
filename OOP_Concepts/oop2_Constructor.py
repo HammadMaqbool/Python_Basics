199e2a68d829bf5743c5d50884c82ac2d0e86a73
HammadMaqbool <hammad_mqbl@outlook.com>
2020-02-29 20:08:35 +0500

    [error-Fixed]Fix in line number 42 print -> input

--- OOP_Concepts/oop2_Constructor.py
+++ OOP_Concepts/oop2_Constructor.py
@@ -3,4 +3,9 @@ class Student:
         self.roll_number = roll_number;
         self.class_ = class_
 
+    def DetailProvider(self):
+        return f"Name is {self} roll number is {self.roll_number} and class is {self.class_}"
+
 Hammad = Student(6,"BSCS"); #Here Self parameter take Hammad automatically and 2nd one is rollnunber 3td is class we provided
+
+print(Hammad.DetailProvider())
