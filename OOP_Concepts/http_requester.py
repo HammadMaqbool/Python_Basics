25be3bce97858a3dbc71eaa3420d1dd39e401798
HammadMaqbool <hammad_mqbl@outlook.com>
2020-03-06 17:07:13 +0500

    Code to play news working Fine

--- OOP_Concepts/http_requester.py
+++ OOP_Concepts/http_requester.py
@@ -1,5 +1,11 @@
-import requests
-r = requests.get("https://financialmodelingprep.com/api/v3/stock/real-time-price/AAPL")
-var =r.content
-var2 = var.split()
-print(var2[6])
+# import requests
+# import json
+# r = requests.get("https://financialmodelingprep.com/api/v3/stock/real-time-price/AAPL")
+# var =r.content
+# print(var)
+# parsed_data = json.loads(var)
+# print(parsed_data['symbol'])
+#
+# # var2 = var.split()
+# # print(var2[6])
+
--- Voice_play.py
+++ Voice_play.py
@@ -1,7 +1,25 @@
+import json
+import requests
+import time
+from newsapi import NewsApiClient
+
 def speak(str):
     from win32com.client import Dispatch
     speak=Dispatch("SAPI.spVoice")
     speak.Speak(str)
 
-MyString = "Please read this string"
-speak(MyString)
+newsapi = NewsApiClient(api_key='ae3a2ade838a47ecabd3d7d5400cb9ab')
+#source = newsapi.get_sources()
+#print(source)
+get_r = requests.get('http://newsapi.org/v2/everything?q=Pakistan&apiKey=ae3a2ade838a47ecabd3d7d5400cb9ab')
+var_text = get_r.text
+parsedData = json.loads(var_text)
+
+for i in range(2):
+    print(parsedData['articles'][i]['title'])
+    speak(parsedData['articles'][i]['title'])
+    time.sleep(1)
+    speak(parsedData['articles'][i]['description'])
+    time.sleep(2)
+myMessage="Thank you for listening news Mr.Hammmad"
+speak(myMessage)
